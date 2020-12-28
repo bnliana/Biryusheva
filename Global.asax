9b85f48f87b510e1972a0c876b2b2bef20516fab
Лиана <75756420+bnliana@users.noreply.github.com>
2020-12-28 20:23:38 +0300

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LianaBirushevaWeb.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LianaBirushevaWeb.Startup))]
+namespace LianaBirushevaWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
